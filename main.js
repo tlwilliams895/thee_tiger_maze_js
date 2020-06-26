4e828b7931d247792aa376b9a8a20a0ff5d35fec
Tyisha Williams <tlwilliams895@gmail.com>
2020-06-26 14:45:42 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Use Multiplication Table and Keydown Events assessments
+const map = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW",
+];
